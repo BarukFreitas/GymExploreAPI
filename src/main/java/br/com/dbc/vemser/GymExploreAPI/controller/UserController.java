56a21b15a2d4c9de5458ff8eb262a44f03af3e01
Baruk Freitas <baruk.freitas@gmail.com>
2025-06-26 09:33:04 -0300

    regra de negocio

--- src/main/java/br/com/dbc/vemser/GymExploreAPI/controller/UserController.java
+++ src/main/java/br/com/dbc/vemser/GymExploreAPI/controller/UserController.java
@@ -1,8 +1,7 @@
 package br.com.dbc.vemser.GymExploreAPI.controller;
 
-import br.com.dbc.vemser.GymExploreAPI.dto.UserLoginDTO; // Importe o DTO
-import br.com.dbc.vemser.GymExploreAPI.dto.UserRegisterDTO; // Importe o DTO
-import br.com.dbc.vemser.GymExploreAPI.dto.UserResponseDTO; // Importe o DTO
+import br.com.dbc.vemser.GymExploreAPI.entity.UserEntity;
+import br.com.dbc.vemser.GymExploreAPI.exception.RegraDeNegocioException;
 import br.com.dbc.vemser.GymExploreAPI.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,8 +11,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-// Para validação (se quiser adicionar)
-// import javax.validation.Valid;
+import javax.validation.Valid;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,36 +25,25 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/register")
-    public ResponseEntity<?> registerUser(@RequestBody /* @Valid */ UserRegisterDTO userRegisterDTO) {
-        try {
-            UserResponseDTO registeredUserDTO = userService.registerUser(userRegisterDTO);
-            Map<String, Object> response = new HashMap<>();
-            response.put("id", registeredUserDTO.getId());
-            response.put("username", registeredUserDTO.getUsername());
-            response.put("email", registeredUserDTO.getEmail());
-            response.put("message", "Usuário registrado com sucesso!");
-            return new ResponseEntity<>(response, HttpStatus.CREATED);
-        } catch (RuntimeException e) {
-            Map<String, String> errorResponse = new HashMap<>();
-            errorResponse.put("error", e.getMessage());
-            return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
-        }
+    public ResponseEntity<Map<String, Object>> registerUser(@RequestBody @Valid UserEntity user) throws RegraDeNegocioException {
+        UserEntity registeredUser = userService.registerUser(user);
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", registeredUser.getId());
+        response.put("username", registeredUser.getUsername());
+        response.put("email", registeredUser.getEmail());
+        response.put("message", "Usuário registrado com sucesso!");
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
-    public ResponseEntity<?> loginUser(@RequestBody UserLoginDTO userLoginDTO) {
-        Optional<UserResponseDTO> loggedInUserDTO = userService.loginUser(userLoginDTO.getUsername(), userLoginDTO.getPassword());
-        if (loggedInUserDTO.isPresent()) {
-            Map<String, Object> response = new HashMap<>();
-            response.put("id", loggedInUserDTO.get().getId());
-            response.put("username", loggedInUserDTO.get().getUsername());
-            response.put("email", loggedInUserDTO.get().getEmail());
-            response.put("message", "Login bem-sucedido!");
-            return new ResponseEntity<>(response, HttpStatus.OK);
-        } else {
-            Map<String, String> errorResponse = new HashMap<>();
-            errorResponse.put("error", "Nome de usuário ou senha inválidos.");
-            return new ResponseEntity<>(errorResponse, HttpStatus.UNAUTHORIZED);
-        }
+    public ResponseEntity<Map<String, Object>> loginUser(@RequestBody UserEntity user) throws RegraDeNegocioException {
+        Optional<UserEntity> loggedInUser = userService.loginUser(user.getUsername(), user.getPassword());
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("id", loggedInUser.get().getId());
+        response.put("username", loggedInUser.get().getUsername());
+        response.put("email", loggedInUser.get().getEmail());
+        response.put("message", "Login bem-sucedido!");
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 }
--- src/main/java/br/com/dbc/vemser/GymExploreAPI/service/UserService.java
+++ src/main/java/br/com/dbc/vemser/GymExploreAPI/service/UserService.java
@@ -1,9 +1,7 @@
 package br.com.dbc.vemser.GymExploreAPI.service;
 
-import br.com.dbc.vemser.GymExploreAPI.dto.UserLoginDTO;
-import br.com.dbc.vemser.GymExploreAPI.dto.UserRegisterDTO;
-import br.com.dbc.vemser.GymExploreAPI.dto.UserResponseDTO;
 import br.com.dbc.vemser.GymExploreAPI.entity.UserEntity;
+import br.com.dbc.vemser.GymExploreAPI.exception.RegraDeNegocioException;
 import br.com.dbc.vemser.GymExploreAPI.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -16,56 +14,30 @@ public class UserService {
     @Autowired
     private UserRepository userRepository;
 
-    public UserResponseDTO registerUser(UserRegisterDTO userRegisterDTO) {
-        if (userRepository.existsByUsername(userRegisterDTO.getUsername())) {
-            throw new RuntimeException("Nome de usuário já existe.");
+    public UserEntity registerUser(UserEntity user) throws RegraDeNegocioException {
+        if (userRepository.existsByUsername(user.getUsername())) {
+            throw new RegraDeNegocioException("Nome de usuário já existe.");
         }
-        if (userRepository.existsByEmail(userRegisterDTO.getEmail())) {
-            throw new RuntimeException("E-mail já cadastrado.");
+        if (userRepository.existsByEmail(user.getEmail())) {
+            throw new RegraDeNegocioException("E-mail já cadastrado.");
         }
-
-        // Mapeia DTO para Entidade
-        UserEntity userEntity = new UserEntity();
-        userEntity.setUsername(userRegisterDTO.getUsername());
-        userEntity.setEmail(userRegisterDTO.getEmail());
-        userEntity.setPassword(userRegisterDTO.getPassword()); // Lembre-se de HASHEAR a senha em produção!
-
-        UserEntity savedUser = userRepository.save(userEntity);
-
-        // Mapeia Entidade para DTO de resposta
-        UserResponseDTO userResponseDTO = new UserResponseDTO();
-        userResponseDTO.setId(savedUser.getId());
-        userResponseDTO.setUsername(savedUser.getUsername());
-        userResponseDTO.setEmail(savedUser.getEmail());
-        return userResponseDTO;
+        return userRepository.save(user);
     }
 
-    public Optional<UserResponseDTO> loginUser(String username, String password) {
-        Optional<UserEntity> userEntityOptional = userRepository.findByUsername(username);
-
-        if (userEntityOptional.isPresent()) {
-            UserEntity userEntity = userEntityOptional.get();
-            // Em produção: usar BCryptPasswordEncoder.matches(rawPassword, encodedPassword)
-            if (userEntity.getPassword().equals(password)) {
-                UserResponseDTO userResponseDTO = new UserResponseDTO();
-                userResponseDTO.setId(userEntity.getId());
-                userResponseDTO.setUsername(userEntity.getUsername());
-                userResponseDTO.setEmail(userEntity.getEmail());
-                return Optional.of(userResponseDTO);
-            }
+    public Optional<UserEntity> loginUser(String username, String password) throws RegraDeNegocioException {
+        Optional<UserEntity> userOptional = userRepository.findByUsername(username);
+        if (userOptional.isEmpty()) {
+            throw new RegraDeNegocioException("Nome de usuário ou senha inválidos.");
         }
-        return Optional.empty();
+
+        UserEntity user = userOptional.get();
+        if (!user.getPassword().equals(password)) {
+            throw new RegraDeNegocioException("Nome de usuário ou senha inválidos.");
+        }
+        return Optional.of(user);
     }
 
-    // Este método pode não ser necessário se o login retornar todas as informações relevantes
-    public Optional<UserResponseDTO> findByUsername(String username) {
-        return userRepository.findByUsername(username)
-                .map(userEntity -> {
-                    UserResponseDTO dto = new UserResponseDTO();
-                    dto.setId(userEntity.getId());
-                    dto.setUsername(userEntity.getUsername());
-                    dto.setEmail(userEntity.getEmail());
-                    return dto;
-                });
+    public Optional<UserEntity> findByUsername(String username) {
+        return userRepository.findByUsername(username);
     }
 }
