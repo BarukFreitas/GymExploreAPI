3344b81305b0f66a45a39c43793715b540962429
IBORD <netinho.rec@hotmail.com>
2025-07-02 15:48:38 -0300

    feat: testes email service e validacao

--- src/main/java/br/com/dbc/vemser/GymExploreAPI/dto/ContactDTO.java
+++ src/main/java/br/com/dbc/vemser/GymExploreAPI/dto/ContactDTO.java
@@ -1,11 +1,21 @@
 package br.com.dbc.vemser.GymExploreAPI.dto;
 
 import lombok.Data;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class ContactDTO {
+    @NotBlank(message = "O nome não pode estar em branco")
     private String name;
+
+    @NotBlank(message = "O e-mail não pode estar em branco")
+    @Email(message = "O formato do e-mail é inválido")
     private String email;
+
+    @NotBlank(message = "O assunto não pode estar em branco")
     private String subject;
+
+    @NotBlank(message = "A mensagem não pode estar em branco")
     private String message;
 }
